ae690ba63b4d6b4cfe02ae925e5a32a80c0e760e
arommers <arommers@student.codam.nl>
2023-10-28 20:23:12 +0200

    equality operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,22 @@
 
-let js = "amazing";
-if (js === "amazing")
-  alert("JS is fun!");
-console.log( + 8 + 23 - 10);
+// let js = "amazing";
+// if (js === "amazing")
+//   alert("JS is fun!");
+// console.log( + 8 + 23 - 10);
+
+// 5 falsy values 0, "", undefined, null, NaN
+
+const age = '18';
+if (age === 18)
+  console.log("strict");
+if (age == 18)
+  console.log("loose");
+
+const fav = prompt("What's your fav number?");
+
+ console.log(fav);
+
+if (fav == 23)
+  console.log("loose")
+if (fav === 23)
+  console.log("strict");

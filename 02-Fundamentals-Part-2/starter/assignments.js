bbcbb2521316f4dba9ba15aae0348e2934a23b45
arommers <arommers@student.codam.nl>
2023-10-29 20:20:03 +0100

    intro to arrays

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -39,10 +39,10 @@
 // let holland = 18;
 // let sweden = 6;
 
-// function percentageOfWorld1(population)
-// {
-//     return ((population / 7900) * 100);
-// }
+function percentageOfWorld1(population)
+{
+    return ((population / 7900) * 100);
+}
 
 // // china = percentageOfWorld1(china);
 // // holland = percentageOfWorld1(holland);
@@ -118,3 +118,23 @@ checkWinner(scoreDolphins, scoreKoalas);
 
 /*======================================================================*/
 
+// LECTURE: Introduction to Arrays
+// 1. Create an array containing 4 population values of 4 countries of your choice. 
+//    You may use the values you have been using previously. 
+//    Store this array into a variable called 'populations'
+// 2. Log to the console whether the array has 4elements or not(trueorfalse)
+// 3. Create an array called 'percentages' containing the percentages of the
+//    world population for these 4 population values. 
+//    Use the function 'percentageOfWorld1'
+//    that you created earlier to compute the 4 percentage values
+
+const populations = [1441, 241, 18, 5];
+console.log (populations.length === 5);
+
+const percentages = [
+percentageOfWorld1(populations[0]),
+percentageOfWorld1(populations[1]),
+percentageOfWorld1(populations[2]),
+percentageOfWorld1(populations[populations.length -1])
+];
+console.log(percentages);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -88,19 +88,62 @@
 
 // Reviewing functions
 
-const calcAge = function (birthYear)
+// const calcAge = function (birthYear)
+// {
+//     return (2037 - birthYear);
+// }
+
+// const yearsUntilRetirement = function(birthYear, firstName){
+//     const age = calcAge(birthYear);
+//     const retirement= 65 - age;
+//     if (retirement > 0)
+//         return (retirement);
+//     else
+//         return (-1);
+// }
+
+// console.log(yearsUntilRetirement(1984, 'Adri'));
+// console.log(yearsUntilRetirement(1940, 'Snoet'));
+
+// intro to arrays
+
+const friend1 = 'john';
+const friend2 = 'bob';
+const friend3 = 'keith';
+
+const friends =['michael', 'bob', 'keith'];
+console.log(friends);
+
+// const years = new Array(1991, 1992, 1993);
+// console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length -1]);
+
+friends[2] = 'jay';
+console.log(friends);
+
+const firstName = 'adri';
+const adri = [firstName, 'Rommers', 2023 - 1984, 'student', friends];
+console.log(adri);
+console.log(adri.length);
+
+// Exercise
+
+const calcAge = function(birthYear)
 {
     return (2037 - birthYear);
 }
 
-const yearsUntilRetirement = function(birthYear, firstName){
-    const age = calcAge(birthYear);
-    const retirement= 65 - age;
-    if (retirement > 0)
-        return (retirement);
-    else
-        return (-1);
-}
+const years = [1990, 1967, 2002, 2010, 2018];
 
-console.log(yearsUntilRetirement(1984, 'Adri'));
-console.log(yearsUntilRetirement(1940, 'Snoet'));
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length -1]);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length -1])];
+console.log(ages);
+
+console.log(age1, age2, age3);

2366d0c8eac630471cdec908f2f447be2a673d5e
arommers <arommers@student.codam.nl>
2023-10-29 12:10:10 +0100

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,34 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest)
-    hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive");
+// if (passTest)
+//     hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
 
 // const interface = 'Audio'
+
+// Calling, running or invoking the function
+
+function logger()
+{
+    console.log("My name is Adri");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges)
+{
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return (juice);
+}
+
+const applejuice = fruitProcessor(5, 0);
+console.log(applejuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);

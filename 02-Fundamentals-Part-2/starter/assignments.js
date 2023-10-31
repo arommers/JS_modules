5d58b5c08e05be3426d621a865b08a618a05dc71
arommers <arommers@student.codam.nl>
2023-10-31 23:12:19 +0100

    methods

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -195,14 +195,14 @@
 // properties 'country', 'capital', 'language', 'population' and
 // 'neighbours' (an array like we used in previous assignments)
 
-const myCountry =
-{
-    country: 'The Netherlands',
-    capital: 'Amsterdam',
-    language: 'Dutch',
-    population: 18,
-    neighbours: ['Germany', 'Belgium'],
-};
+// const myCountry =
+// {
+//     country: 'The Netherlands',
+//     capital: 'Amsterdam',
+//     language: 'Dutch',
+//     population: 18,
+//     neighbours: ['Germany', 'Belgium'],
+// };
 
 // console.log(myCountry);
 
@@ -215,14 +215,47 @@ const myCountry =
 // 2. Increase the country's population by two million using dot notation, and then
 // decrease it by two million using brackets notation.
 
-console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language} speaking people,
-${myCountry.neighbours.length} neigbouring countries and a capital called ${myCountry.capital}.`);
+// console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language} speaking people,
+// ${myCountry.neighbours.length} neigbouring countries and a capital called ${myCountry.capital}.`);
 
-myCountry.population += 2;
-console.log(myCountry);
-myCountry['population'] -= 2;
-console.log(myCountry);
+// myCountry.population += 2;
+// console.log(myCountry);
+// myCountry['population'] -= 2;
+// console.log(myCountry);
 
+/*======================================================================*/
 
+// LECTURE: Object Methods
+// 1. Add a method called 'describe' to the 'myCountry' object. This method
+// will log a string to the console, similar to the string logged in the previous
+// assignment, but this time using the 'this' keyword.
+// 2. Call the 'describe' method
+// 3. Add a method called 'checkIsland' to the 'myCountry' object. This
+// method will set a new property on the object, called 'isIsland'.
+// 'isIsland' will be true if there are no neighbouring countries, and false if
+// there are. Use the ternary operator to set the property.
+
+const myCountry =
+{
+    country: 'The Netherlands',
+    capital: 'Amsterdam',
+    language: 'Dutch',
+    population: 18,
+    neighbours: ['Germany', 'Belgium'],
+
+    describe: function()
+    {
+        console.log(`${this.country} has ${this.population} million ${this.language} speaking people,
+        ${this.neighbours.length} neigbouring countries and a capital called ${this.capital}.`);
+    },
+    checkIsland: function()
+    {
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+    }
+};
+
+myCountry.describe();
+myCountry.checkIsland();
+console.log(myCountry.isIsland);
 
 /*======================================================================*/

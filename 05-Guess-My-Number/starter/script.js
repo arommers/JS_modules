b9f9bd0305d9f95ac4d7eb654f7a425b883e7f60
arommers <arommers@student.codam.nl>
2023-11-07 22:53:48 +0100

    started guess game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,21 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = ' 🥳 Correct Number!';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = ' 🥳 Correct Number!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 14;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 14;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener
+('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Input ⛔️';
+    }
+});

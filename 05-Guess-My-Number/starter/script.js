b27d60716620c983fa2c0e1ea5ea33556e332f0b
arommers <arommers@student.codam.nl>
2023-11-08 22:05:29 +0100

    74

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,12 +10,42 @@
 // document.querySelector('.guess').value = 14;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener
 ('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if (!guess) {
-        document.querySelector('.message').textContent = 'No Input ⛔️';
+        document.querySelector('.message').textContent = ' ⛔️ No Input';
+    }
+    else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = ' 🥳 Correct Number!';
+    }
+    else if (guess > secretNumber) {
+        if (score > 1) {
+            score--;
+            document.querySelector('.message').textContent = ' 📈 Too high';
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = ' 💥 You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+    else if (guess < secretNumber) {
+        if (score > 1) {
+            score--;
+            document.querySelector('.message').textContent = ' 📉 Too low';
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = ' 💥 You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });

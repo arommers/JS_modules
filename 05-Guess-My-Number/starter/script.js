28e7aaa0d06528f9f653dabb629283c5f8ef1e90
arommers <arommers@student.codam.nl>
2023-11-09 21:55:03 +0100

    added reset functionalities

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,22 +10,27 @@
 // document.querySelector('.guess').value = 14;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.check').addEventListener
-('click', function() {
+
+document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-
+    
+    //  when there is no input
     if (!guess) {
         document.querySelector('.message').textContent = ' ⛔️ No Input';
     }
+    // when the guess is correct
     else if (guess === secretNumber) {
         document.querySelector('.message').textContent = ' 🥳 Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
     }
+    // when guess is too high
     else if (guess > secretNumber) {
         if (score > 1) {
             score--;
@@ -37,6 +42,7 @@ document.querySelector('.check').addEventListener
             document.querySelector('.score').textContent = 0;
         }
     }
+    // when guess is too low
     else if (guess < secretNumber) {
         if (score > 1) {
             score--;
@@ -48,4 +54,15 @@ document.querySelector('.check').addEventListener
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222222';
+    document.querySelector('.guess').value = '';
 });

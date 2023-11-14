8f9ea53bed2064b1281f21317d55eafb25dccab9
arommers <arommers@student.codam.nl>
2023-11-14 21:40:17 +0100

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if(birthYear >= 1981 && birthYear <= 1996) {
+        var millenial = true;
+        // creating new variable with same name as outer scope variable
+        const firstName = 'Steven';
+        const string = `OH, and you're a millenial ${firstName}`;
+        console.log(string);
+
+        function add(a, b) {
+            return (a + b);
+        }
+        // reassinging outeer scope variable with new value
+        output = 'NEW OUTPUT';
+    }
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Adri';
+calcAge(1991);

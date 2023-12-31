8fa85d4360280bc162ab2402e73a1b537bc012e7
arommers <arommers@student.codam.nl>
2023-12-31 13:59:14 +0100

    started maps in JS

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -41,53 +41,82 @@ const restaurant = {
   },
 };
 
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
 
-console.log(ordersSet);
-console.log(new Set('Jonas'));
+// console.log(ordersSet);
+// console.log(new Set('Jonas'));
 
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-ordersSet.delete('Risotto');
-// ordersSet.clear();
-console.log(ordersSet);
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// ordersSet.delete('Risotto');
+// // ordersSet.clear();
+// console.log(ordersSet);
 
-for (const order of ordersSet) console.log(order);
+// for (const order of ordersSet) console.log(order);
 
-// Example removing duplicates from an array
+// // Example removing duplicates from an array
 
-const staff = [
-  'Waiter',
-  'Chef',
-  'Waiter',
-  'Manager',
-  'Chef',
-  'Waiter'
-];
+// const staff = [
+//   'Waiter',
+//   'Chef',
+//   'Waiter',
+//   'Manager',
+//   'Chef',
+//   'Waiter'
+// ];
 
-const staffList = [...new Set (staff)];
-console.log(staff);
-console.log(staffList);
-console.log(new Set([
-  'Waiter',
-  'Chef',
-  'Waiter',
-  'Manager',
-  'Chef',
-  'Waiter'
-]).size);
+// const staffList = [...new Set (staff)];
+// console.log(staff);
+// console.log(staffList);
+// console.log(new Set([
+//   'Waiter',
+//   'Chef',
+//   'Waiter',
+//   'Manager',
+//   'Chef',
+//   'Waiter'
+// ]).size);
 
-console.log(new Set('Adri').size);
+// console.log(new Set('Adri').size);
+
+const rest = new Map();
+rest.set('Name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+console.log(rest);
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed')
+
+console.log(rest);
+console.log(rest.get(true));
+console.log(rest.get('Name'));
+
+const time = 21;
+
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+// rest.set([1, 2], 'Test')
 
 // const rest1 = {
 //   name: 'Poop',

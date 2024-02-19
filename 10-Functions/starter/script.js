71f79e6b654bd7b88a16f0d761c037ac71707169
arommers <arommers@codam.student.nl>
2024-02-19 11:20:24 +0100

    10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -179,3 +179,39 @@ const flightData = [583, 'George'];
 book.apply(swiss, flightData);
 book.call(swiss, ...flightData);
 console.log(swiss);
+
+// Bind method
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(123, 'Stephen Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Adri Rommers');
+bookEW23('Poesesich');
+
+// With Event Listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function()
+{
+    console.log(this);
+    this.planes++;
+    console.log(this.planes);
+}
+// lufthansa.buyPlane();
+
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVat = addTax.bind(null, 0.23);
+// const addTax = (rate, value) => value + value * 0.23;
+
+console.log(addVat(100));
+console.log(addVat(23));
